e2ccb1a8c7c3a6b3651116b3e150ca083db9199a
Christian Poitras <christian.poitras@ircm.qc.ca>
2016-03-24 15:39:58 -0400

    Use import static in RegisterPresenter.

--- src/main/java/ca/qc/ircm/proview/user/web/RegisterPresenter.java
+++ src/main/java/ca/qc/ircm/proview/user/web/RegisterPresenter.java
@@ -1,38 +1,32 @@
 package ca.qc.ircm.proview.user.web;
 
-import ca.qc.ircm.proview.laboratory.QLaboratory;
-import ca.qc.ircm.proview.user.QAddress;
-import ca.qc.ircm.proview.user.QPhoneNumber;
-import ca.qc.ircm.proview.user.QUser;
+import static ca.qc.ircm.proview.laboratory.QLaboratory.laboratory;
+import static ca.qc.ircm.proview.user.QAddress.address1;
+import static ca.qc.ircm.proview.user.QPhoneNumber.phoneNumber;
+import static ca.qc.ircm.proview.user.QUser.user;
 
 /**
  * Registers user presenter.
  */
 public interface RegisterPresenter {
-  public static final String emailProperty = QUser.user.email.getMetadata().getName();
-  public static final String nameProperty = QUser.user.name.getMetadata().getName();
-  public static final String laboratoryNameProperty =
-      QLaboratory.laboratory.name.getMetadata().getName();
-  public static final String organizationProperty =
-      QLaboratory.laboratory.organization.getMetadata().getName();
-  public static final String addressProperty = QAddress.address1.address.getMetadata().getName();
-  public static final String addressSecondProperty =
-      QAddress.address1.addressSecond.getMetadata().getName();
-  public static final String townProperty = QAddress.address1.town.getMetadata().getName();
-  public static final String stateProperty = QAddress.address1.state.getMetadata().getName();
-  public static final String countryProperty = QAddress.address1.country.getMetadata().getName();
-  public static final String postalCodeProperty =
-      QAddress.address1.postalCode.getMetadata().getName();
-  public static final String phoneNumberProperty =
-      QPhoneNumber.phoneNumber.number.getMetadata().getName();
-  public static final String phoneExtensionProperty =
-      QPhoneNumber.phoneNumber.extension.getMetadata().getName();
+  public static final String emailProperty = user.email.getMetadata().getName();
+  public static final String nameProperty = user.name.getMetadata().getName();
+  public static final String laboratoryNameProperty = laboratory.name.getMetadata().getName();
+  public static final String organizationProperty = laboratory.organization.getMetadata().getName();
+  public static final String addressProperty = address1.address.getMetadata().getName();
+  public static final String addressSecondProperty = address1.addressSecond.getMetadata().getName();
+  public static final String townProperty = address1.town.getMetadata().getName();
+  public static final String stateProperty = address1.state.getMetadata().getName();
+  public static final String countryProperty = address1.country.getMetadata().getName();
+  public static final String postalCodeProperty = address1.postalCode.getMetadata().getName();
+  public static final String phoneNumberProperty = phoneNumber.number.getMetadata().getName();
+  public static final String phoneExtensionProperty = phoneNumber.extension.getMetadata().getName();
   public static final String passwordProperty = "password";
   public static final String confirmPasswordProperty = "confirmPassword";
 
   /**
    * Called by view when view is initialized.
-   * 
+   *
    * @param view
    *          view
    */
