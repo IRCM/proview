893b6b3915ee96a32ec3c859c07c42f2714af294
Christian Poitras <christian.poitras@ircm.qc.ca>
2017-03-16 18:12:05 -0400

    Use Chrome by default for TestBench on MacOS.

--- src/test/java/ca/qc/ircm/proview/test/config/TestBenchTestExecutionListener.java
+++ src/test/java/ca/qc/ircm/proview/test/config/TestBenchTestExecutionListener.java
@@ -19,9 +19,10 @@ package ca.qc.ircm.proview.test.config;
 
 import static org.junit.Assume.assumeTrue;
 
+import com.vaadin.testbench.Parameters;
 import com.vaadin.testbench.TestBenchTestCase;
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -44,7 +45,11 @@ public class TestBenchTestExecutionListener extends AbstractTestExecutionListene
       new String[] { "vaadin.testbench.developer.license", ".vaadin.testbench.developer.license" };
   private static final String LICENSE_SYSTEM_PROPERTY = "vaadin.testbench.developer.license";
   private static final String SKIP_TESTS_ERROR_MESSAGE = "TestBench tests are skipped";
-  private static final String SKIP_TESTS_SYSTEM_PROPERTY = "testbench-tests.skip";
+  private static final String SKIP_TESTS_SYSTEM_PROPERTY = "testbench.skip";
+  private static final String DRIVER_SYSTEM_PROPERTY = "testbench.driver";
+  private static final String RETRIES_SYSTEM_PROPERTY = "testbench.retries";
+  private static final String DEFAULT_DRIVER = "org.openqa.selenium.firefox.FirefoxDriver";
+  private static final String CHROME_DRIVER = "org.openqa.selenium.chrome.ChromeDriver";
   private static final Logger logger =
       LoggerFactory.getLogger(TestBenchTestExecutionListener.class);
 
@@ -66,13 +71,14 @@ public class TestBenchTestExecutionListener extends AbstractTestExecutionListene
         logger.info(message);
         assumeTrue(message, false);
       }
+      setRetries();
     }
   }
 
   @Override
   public void beforeTestMethod(TestContext testContext) throws Exception {
     if (isTestBenchTest(testContext)) {
-      WebDriver driver = new FirefoxDriver();
+      WebDriver driver = driver();
       TestBenchTestCase target = getInstance(testContext);
       target.setDriver(driver);
     }
@@ -97,6 +103,30 @@ public class TestBenchTestExecutionListener extends AbstractTestExecutionListene
 
   private TestBenchTestCase getInstance(TestContext testContext) {
     return (TestBenchTestCase) testContext.getTestInstance();
+  }
+
+  private WebDriver driver() {
+    String driverClass = System.getProperty(DRIVER_SYSTEM_PROPERTY);
+    if (driverClass == null) {
+      driverClass = DEFAULT_DRIVER;
+      if (SystemUtils.IS_OS_MAC_OSX) {
+        // Defaults to Chrome on MacOS. See
+        // https://vaadin.com/docs/-/part/testbench/testbench-known-issues.html
+        driverClass = CHROME_DRIVER;
+      }
+    }
+    try {
+      return (WebDriver) Class.forName(driverClass).newInstance();
+    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+      logger.error("Could not instantiate WebDriver class {}", driverClass);
+      throw new IllegalStateException("Could not instantiate WebDriver class " + driverClass, e);
+    }
+  }
+
+  private void setRetries() {
+    if (System.getProperty(RETRIES_SYSTEM_PROPERTY) != null) {
+      Parameters.setMaxAttempts(Integer.parseInt(System.getProperty(RETRIES_SYSTEM_PROPERTY)));
+    }
   }
 
   @Override
