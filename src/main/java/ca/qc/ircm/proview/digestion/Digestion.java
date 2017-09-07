7a3fcc8d6a92595e32e82452652397f60bcfeafc
Christian Poitras <christian.poitras@ircm.qc.ca>
2017-09-07 14:01:09 -0400

    Removes unused Digestion's getDigestedSamples method.

--- src/main/java/ca/qc/ircm/proview/digestion/Digestion.java
+++ src/main/java/ca/qc/ircm/proview/digestion/Digestion.java
@@ -19,8 +19,6 @@ package ca.qc.ircm.proview.digestion;
 
 import ca.qc.ircm.proview.treatment.Treatment;
 
-import java.util.List;
-
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -62,9 +60,5 @@ public class Digestion extends Treatment<DigestedSample> {
 
   public void setProtocol(DigestionProtocol protocol) {
     this.protocol = protocol;
-  }
-
-  public List<DigestedSample> getDigestedSamples() {
-    return getTreatmentSamples();
   }
 }
