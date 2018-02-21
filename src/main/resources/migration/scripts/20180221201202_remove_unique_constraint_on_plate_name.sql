988e0fe359a596827637108308871cfc61c3b40c
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-02-21 15:33:11 -0500

    Removes incorrect unique constraint on plate's name.

---
+++ src/main/resources/migration/scripts/20180221201202_remove_unique_constraint_on_plate_name.sql
@@ -0,0 +1,15 @@
+-- // remove unique constraint on plate name
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE plate
+DROP INDEX name;
+ALTER TABLE plate
+ADD INDEX name (name);
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE plate
+DROP INDEX name;
+ALTER TABLE plate
+ADD CONSTRAINT name UNIQUE (name);
