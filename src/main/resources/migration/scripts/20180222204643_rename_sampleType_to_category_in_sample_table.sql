83d7331d4f0695271aacc67a3313b52c1d25d0e1
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-02-22 16:40:05 -0500

    Improves rename sampleType to category in sample table script.

--- src/main/resources/migration/scripts/20180222204643_rename_sampleType_to_category_in_sample_table.sql
+++ src/main/resources/migration/scripts/20180222204643_rename_sampleType_to_category_in_sample_table.sql
@@ -2,19 +2,11 @@
 -- Migration SQL that makes the change goes here.
 
 ALTER TABLE sample
-ADD COLUMN category varchar(50) DEFAULT NULL AFTER sampleType;
-UPDATE sample
-SET category = sampleType;
-ALTER TABLE sample
-DROP COLUMN sampleType;
+CHANGE COLUMN sampleType category varchar(50) DEFAULT NULL;
 
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
 ALTER TABLE sample
-ADD COLUMN sampleType varchar(50) DEFAULT NULL AFTER category;
-UPDATE sample
-SET sampleType = category;
-ALTER TABLE sample
-DROP COLUMN category;
+CHANGE COLUMN category sampleType varchar(50) DEFAULT NULL;
