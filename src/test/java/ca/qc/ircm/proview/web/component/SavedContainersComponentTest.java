08329ece7f695556055a43c0f108acba2fb0ed67
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-02-26 14:00:16 -0500

    Use repositories in SavedContainersComponentTest.

--- src/test/java/ca/qc/ircm/proview/web/component/SavedContainersComponentTest.java
+++ src/test/java/ca/qc/ircm/proview/web/component/SavedContainersComponentTest.java
@@ -45,34 +45,26 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ca.qc.ircm.proview.sample.SampleContainer;
+import ca.qc.ircm.proview.sample.SampleContainerRepository;
 import ca.qc.ircm.proview.sample.SubmissionSample;
+import ca.qc.ircm.proview.test.config.AbstractComponentTestCase;
 import ca.qc.ircm.proview.test.config.ServiceTestAnnotations;
-import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.ConnectorTracker;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.UI;
 import java.util.ArrayList;
 import java.util.Collection;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ServiceTestAnnotations
-public class SavedContainersComponentTest {
+public class SavedContainersComponentTest extends AbstractComponentTestCase {
   private SavedContainersComponent component;
-  @Mock
-  private UI ui;
-  @Mock
-  private ConnectorTracker connectorTracker;
-  @Mock
-  private VaadinSession session;
-  @PersistenceContext
-  private EntityManager entityManager;
+  @Inject
+  private SampleContainerRepository repository;
   private Collection<SampleContainer> containers = new ArrayList<>();
   private SampleContainer container1;
   private SampleContainer container2;
@@ -82,9 +74,6 @@ public class SavedContainersComponentTest {
    */
   @Before
   public void beforeTest() {
-    when(ui.getConnectorTracker()).thenReturn(connectorTracker);
-    when(ui.getSession()).thenReturn(session);
-    when(session.hasLock()).thenReturn(true);
     component = new TestComponent();
     containers.add(container1);
     containers.add(container2);
@@ -95,12 +84,12 @@ public class SavedContainersComponentTest {
     component.saveContainers(containers);
 
     verify(ui, atLeastOnce()).getSession();
-    verify(session).setAttribute(SAVED_CONTAINERS, containers);
+    verify(vaadinSession).setAttribute(SAVED_CONTAINERS, containers);
   }
 
   @Test
   public void savedContainers() {
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     Collection<SampleContainer> containers = component.savedContainers();
 
@@ -108,23 +97,23 @@ public class SavedContainersComponentTest {
     assertTrue(this.containers.containsAll(containers));
     assertTrue(containers.containsAll(this.containers));
     verify(ui, atLeastOnce()).getSession();
-    verify(session).getAttribute(SAVED_CONTAINERS);
+    verify(vaadinSession).getAttribute(SAVED_CONTAINERS);
   }
 
   @Test
   public void savedContainers_Null() {
-    when(session.getAttribute(any(String.class))).thenReturn(null);
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(null);
 
     Collection<SampleContainer> containers = component.savedContainers();
 
     assertTrue(containers.isEmpty());
     verify(ui, atLeastOnce()).getSession();
-    verify(session).getAttribute(SAVED_CONTAINERS);
+    verify(vaadinSession).getAttribute(SAVED_CONTAINERS);
   }
 
   @Test
   public void savedContainers_ModifyList() {
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
     final int size = containers.size();
     Collection<SampleContainer> containers = component.savedContainers();
     containers.remove(containers.iterator().next());
@@ -137,11 +126,11 @@ public class SavedContainersComponentTest {
   @Test
   public void savedContainersFromMultipleUsers_True() {
     Collection<SampleContainer> containers = new ArrayList<>();
-    containers.add(entityManager.find(SampleContainer.class, 2L));
-    containers.add(entityManager.find(SampleContainer.class, 3L));
-    containers.add(entityManager.find(SampleContainer.class, 4L));
-    containers.add(entityManager.find(SampleContainer.class, 8L));
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    containers.add(repository.findOne(2L));
+    containers.add(repository.findOne(3L));
+    containers.add(repository.findOne(4L));
+    containers.add(repository.findOne(8L));
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     boolean multipleUsers = component.savedContainersFromMultipleUsers();
 
@@ -151,10 +140,10 @@ public class SavedContainersComponentTest {
   @Test
   public void savedContainersFromMultipleUsers_False() {
     Collection<SampleContainer> containers = new ArrayList<>();
-    containers.add(entityManager.find(SampleContainer.class, 2L));
-    containers.add(entityManager.find(SampleContainer.class, 3L));
-    containers.add(entityManager.find(SampleContainer.class, 4L));
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    containers.add(repository.findOne(2L));
+    containers.add(repository.findOne(3L));
+    containers.add(repository.findOne(4L));
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     boolean multipleUsers = component.savedContainersFromMultipleUsers();
 
@@ -164,13 +153,13 @@ public class SavedContainersComponentTest {
   @Test
   public void savedContainersFromMultipleUsers_FalseDueToNullUser() {
     Collection<SampleContainer> containers = new ArrayList<>();
-    containers.add(entityManager.find(SampleContainer.class, 2L));
-    containers.add(entityManager.find(SampleContainer.class, 3L));
-    containers.add(entityManager.find(SampleContainer.class, 4L));
-    SampleContainer container = entityManager.find(SampleContainer.class, 8L);
+    containers.add(repository.findOne(2L));
+    containers.add(repository.findOne(3L));
+    containers.add(repository.findOne(4L));
+    SampleContainer container = repository.findOne(8L);
     ((SubmissionSample) container.getSample()).getSubmission().setUser(null);
     containers.add(container);
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     boolean multipleUsers = component.savedContainersFromMultipleUsers();
 
@@ -180,11 +169,11 @@ public class SavedContainersComponentTest {
   @Test
   public void savedContainersFromMultipleUsers_FalseDueToNullSample() {
     Collection<SampleContainer> containers = new ArrayList<>();
-    containers.add(entityManager.find(SampleContainer.class, 2L));
-    containers.add(entityManager.find(SampleContainer.class, 3L));
-    containers.add(entityManager.find(SampleContainer.class, 4L));
-    containers.add(entityManager.find(SampleContainer.class, 130L));
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    containers.add(repository.findOne(2L));
+    containers.add(repository.findOne(3L));
+    containers.add(repository.findOne(4L));
+    containers.add(repository.findOne(130L));
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     boolean multipleUsers = component.savedContainersFromMultipleUsers();
 
@@ -194,7 +183,7 @@ public class SavedContainersComponentTest {
   @Test
   public void savedContainersFromMultipleUsers_Emtpy() {
     Collection<SampleContainer> containers = new ArrayList<>();
-    when(session.getAttribute(any(String.class))).thenReturn(containers);
+    when(vaadinSession.getAttribute(any(String.class))).thenReturn(containers);
 
     boolean multipleUsers = component.savedContainersFromMultipleUsers();
 
