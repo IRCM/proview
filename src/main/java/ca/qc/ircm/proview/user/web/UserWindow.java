c4311525caacc2184e1ca5b41458ff167d065355
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-04-12 17:29:31 -0400

    Revert "Allows update of user in UserWindow."
    This reverts commit 912ab085dbdcb572aed6241dae1d111145cf168e.

--- src/main/java/ca/qc/ircm/proview/user/web/UserWindow.java
+++ src/main/java/ca/qc/ircm/proview/user/web/UserWindow.java
@@ -91,11 +91,12 @@ public class UserWindow extends Window implements BaseComponent {
   private void updateUser(User user) {
     logger.debug("User window for user {}", user);
     setCaption(getResources().message(TITLE, user != null ? user.getName() : ""));
+    design.update.setVisible(authorizationService.hasUserWritePermission(user));
     design.update.addClickListener(e -> {
       navigateTo(UserView.VIEW_NAME, String.valueOf(user.getId()));
       close();
     });
     userForm.setValue(user);
-    userForm.setReadOnly(!authorizationService.hasUserWritePermission(user));
+    userForm.setReadOnly(true);
   }
 }
