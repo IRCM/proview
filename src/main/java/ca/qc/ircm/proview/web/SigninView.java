31138d0ec7256900d54471460595816c82d99666
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-06-25 16:32:16 -0400

    Updates SignView page to use login overlay.

--- src/main/java/ca/qc/ircm/proview/web/SigninView.java
+++ src/main/java/ca/qc/ircm/proview/web/SigninView.java
@@ -1,40 +1,119 @@
 package ca.qc.ircm.proview.web;
 
-import com.vaadin.flow.component.Tag;
+import static ca.qc.ircm.proview.text.Strings.property;
+import static ca.qc.ircm.proview.user.UserProperties.EMAIL;
+import static ca.qc.ircm.proview.user.UserProperties.HASHED_PASSWORD;
+import static ca.qc.ircm.proview.web.WebConstants.APPLICATION_NAME;
+import static ca.qc.ircm.proview.web.WebConstants.TITLE;
+
+import ca.qc.ircm.proview.user.User;
+import ca.qc.ircm.utils.MessageResource;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginI18n.ErrorMessage;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.HasDynamicTitle;
+import com.vaadin.flow.router.Route;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * A Designer generated component for the signin-view template.
- *
- * Designer will add and remove fields with @Id mappings but does not overwrite or otherwise change
- * this file.
+ * Signin view.
  */
-@Tag("signin-view")
-@HtmlImport("src/signin-view.html")
-public class SigninView extends PolymerTemplate<SigninView.SigninViewModel> {
+@Route(value = SigninView.VIEW_NAME)
+@HtmlImport("styles/shared-styles.html")
+public class SigninView extends LoginOverlay
+    implements LocaleChangeObserver, HasDynamicTitle, AfterNavigationObserver, BeforeEnterObserver {
   public static final String VIEW_NAME = "signin";
   public static final String HEADER = "header";
+  public static final String DESCRIPTION = "description";
+  public static final String FORM_TITLE = "form.title";
   public static final String USERNAME = "username";
   public static final String PASSWORD = "password";
   public static final String SIGNIN = "signin";
-  public static final String DO_SIGNIN = "dosignin";
   public static final String FAIL = "fail";
   public static final String DISABLED = "disabled";
-  public static final String EXCESSIVE_ATTEMPTS = "excessiveAttempts";
+  public static final String LOCKED = "locked";
+  private static final long serialVersionUID = -3000859016669509494L;
+  @SuppressWarnings("unused")
+  private static final Logger logger = LoggerFactory.getLogger(SigninView.class);
+  protected LoginI18n i18n;
+  protected String error;
 
-  /**
-   * Creates a new SigninView.
-   */
   public SigninView() {
-    // You can initialise any data required for the connected UI components here.
   }
 
-  /**
-   * This model binds properties between SigninView and signin-view
-   */
-  public interface SigninViewModel extends TemplateModel {
-    // Add setters and getters for template properties here.
+  @PostConstruct
+  void init() {
+    setId(VIEW_NAME);
+    addLoginListener(e -> setError(false));
+    setForgotPasswordButtonVisible(false);
+    setAction(VIEW_NAME);
+    setOpened(true);
+  }
+
+  @Override
+  public void beforeEnter(BeforeEnterEvent event) {
+    // TODO Redirect to main view if user is known.
+    /*
+    if (!authorizationService.isAnonymous()) {
+      UI.getCurrent().getPage().getHistory().replaceState(null, "");
+      event.rerouteTo(MainView.class);
+    }
+    */
+  }
+
+  @Override
+  public void localeChange(LocaleChangeEvent event) {
+    final MessageResource resources = new MessageResource(getClass(), getLocale());
+    final MessageResource userResources = new MessageResource(User.class, getLocale());
+    i18n = LoginI18n.createDefault();
+    i18n.setHeader(new LoginI18n.Header());
+    i18n.getHeader().setTitle(resources.message(HEADER));
+    i18n.getHeader().setDescription(resources.message(DESCRIPTION));
+    i18n.setAdditionalInformation(null);
+    i18n.setForm(new LoginI18n.Form());
+    i18n.getForm().setSubmit(resources.message(SIGNIN));
+    i18n.getForm().setTitle(resources.message(FORM_TITLE));
+    i18n.getForm().setUsername(userResources.message(EMAIL));
+    i18n.getForm().setPassword(userResources.message(HASHED_PASSWORD));
+    i18n.setErrorMessage(new ErrorMessage());
+    if (error == null) {
+      error = FAIL;
+    }
+    i18n.getErrorMessage().setTitle(resources.message(property(error, TITLE)));
+    i18n.getErrorMessage().setMessage(resources.message(error));
+    setI18n(i18n);
+  }
+
+  @Override
+  public String getPageTitle() {
+    final MessageResource resources = new MessageResource(getClass(), getLocale());
+    final MessageResource generalResources = new MessageResource(WebConstants.class, getLocale());
+    return resources.message(TITLE, generalResources.message(APPLICATION_NAME));
+  }
+
+  @Override
+  public void afterNavigation(AfterNavigationEvent event) {
+    Map<String, List<String>> parameters = event.getLocation().getQueryParameters().getParameters();
+    if (parameters.containsKey(DISABLED)) {
+      error = DISABLED;
+      setError(true);
+    } else if (parameters.containsKey(LOCKED)) {
+      error = LOCKED;
+      setError(true);
+    } else if (parameters.containsKey(FAIL)) {
+      error = FAIL;
+      setError(true);
+    }
   }
 }
---
+++ src/test/java/ca/qc/ircm/proview/web/SigninViewTest.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2018 Institut de recherches cliniques de Montreal (IRCM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.qc.ircm.proview.web;
+
+import static ca.qc.ircm.proview.text.Strings.property;
+import static ca.qc.ircm.proview.user.UserProperties.EMAIL;
+import static ca.qc.ircm.proview.user.UserProperties.HASHED_PASSWORD;
+import static ca.qc.ircm.proview.web.SigninView.DESCRIPTION;
+import static ca.qc.ircm.proview.web.SigninView.DISABLED;
+import static ca.qc.ircm.proview.web.SigninView.FAIL;
+import static ca.qc.ircm.proview.web.SigninView.FORM_TITLE;
+import static ca.qc.ircm.proview.web.SigninView.HEADER;
+import static ca.qc.ircm.proview.web.SigninView.LOCKED;
+import static ca.qc.ircm.proview.web.SigninView.SIGNIN;
+import static ca.qc.ircm.proview.web.SigninView.VIEW_NAME;
+import static ca.qc.ircm.proview.web.WebConstants.APPLICATION_NAME;
+import static ca.qc.ircm.proview.web.WebConstants.TITLE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import ca.qc.ircm.proview.test.config.AbstractViewTestCase;
+import ca.qc.ircm.proview.test.config.NonTransactionalTestAnnotations;
+import ca.qc.ircm.proview.user.User;
+import ca.qc.ircm.utils.MessageResource;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.Location;
+import com.vaadin.flow.router.QueryParameters;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@NonTransactionalTestAnnotations
+public class SigninViewTest extends AbstractViewTestCase {
+  private SigninView view;
+  @Mock
+  private AfterNavigationEvent afterNavigationEvent;
+  @Mock
+  private Location location;
+  @Mock
+  private QueryParameters queryParameters;
+  private Locale locale = Locale.ENGLISH;
+  private MessageResource resources = new MessageResource(SigninView.class, locale);
+  private MessageResource userResources = new MessageResource(User.class, locale);
+  private MessageResource generalResources = new MessageResource(WebConstants.class, locale);
+  private Map<String, List<String>> parameters = new HashMap<>();
+
+  /**
+   * Before test.
+   */
+  @Before
+  public void beforeTest() {
+    when(ui.getLocale()).thenReturn(locale);
+    view = new SigninView();
+    view.init();
+    when(afterNavigationEvent.getLocation()).thenReturn(location);
+    when(location.getQueryParameters()).thenReturn(queryParameters);
+    when(queryParameters.getParameters()).thenReturn(parameters);
+  }
+
+  @Test
+  public void styles() {
+    assertTrue(view.getId().orElse("").equals(VIEW_NAME));
+    assertFalse(view.isForgotPasswordButtonVisible());
+  }
+
+  @Test
+  public void labels() {
+    view.localeChange(mock(LocaleChangeEvent.class));
+    assertEquals(resources.message(HEADER), view.i18n.getHeader().getTitle());
+    assertEquals(resources.message(DESCRIPTION), view.i18n.getHeader().getDescription());
+    assertEquals(resources.message(FORM_TITLE), view.i18n.getForm().getTitle());
+    assertEquals(userResources.message(EMAIL), view.i18n.getForm().getUsername());
+    assertEquals(userResources.message(HASHED_PASSWORD), view.i18n.getForm().getPassword());
+    assertEquals(resources.message(SIGNIN), view.i18n.getForm().getSubmit());
+    assertEquals(resources.message(property(FAIL, TITLE)), view.i18n.getErrorMessage().getTitle());
+    assertEquals(resources.message(FAIL), view.i18n.getErrorMessage().getMessage());
+  }
+
+  @Test
+  public void labels_Fail() {
+    parameters.put(FAIL, null);
+    view.afterNavigation(afterNavigationEvent);
+    view.localeChange(mock(LocaleChangeEvent.class));
+    assertEquals(resources.message(HEADER), view.i18n.getHeader().getTitle());
+    assertEquals(resources.message(DESCRIPTION), view.i18n.getHeader().getDescription());
+    assertEquals(resources.message(FORM_TITLE), view.i18n.getForm().getTitle());
+    assertEquals(userResources.message(EMAIL), view.i18n.getForm().getUsername());
+    assertEquals(userResources.message(HASHED_PASSWORD), view.i18n.getForm().getPassword());
+    assertEquals(resources.message(SIGNIN), view.i18n.getForm().getSubmit());
+    assertEquals(resources.message(property(FAIL, TITLE)), view.i18n.getErrorMessage().getTitle());
+    assertEquals(resources.message(FAIL), view.i18n.getErrorMessage().getMessage());
+  }
+
+  @Test
+  public void labels_Disabled() {
+    parameters.put(DISABLED, null);
+    view.afterNavigation(afterNavigationEvent);
+    view.localeChange(mock(LocaleChangeEvent.class));
+    assertEquals(resources.message(HEADER), view.i18n.getHeader().getTitle());
+    assertEquals(resources.message(DESCRIPTION), view.i18n.getHeader().getDescription());
+    assertEquals(resources.message(FORM_TITLE), view.i18n.getForm().getTitle());
+    assertEquals(userResources.message(EMAIL), view.i18n.getForm().getUsername());
+    assertEquals(userResources.message(HASHED_PASSWORD), view.i18n.getForm().getPassword());
+    assertEquals(resources.message(SIGNIN), view.i18n.getForm().getSubmit());
+    assertEquals(resources.message(property(DISABLED, TITLE)),
+        view.i18n.getErrorMessage().getTitle());
+    assertEquals(resources.message(DISABLED), view.i18n.getErrorMessage().getMessage());
+  }
+
+  @Test
+  public void labels_Locked() {
+    parameters.put(LOCKED, null);
+    view.afterNavigation(afterNavigationEvent);
+    view.localeChange(mock(LocaleChangeEvent.class));
+    assertEquals(resources.message(HEADER), view.i18n.getHeader().getTitle());
+    assertEquals(resources.message(DESCRIPTION), view.i18n.getHeader().getDescription());
+    assertEquals(resources.message(FORM_TITLE), view.i18n.getForm().getTitle());
+    assertEquals(userResources.message(EMAIL), view.i18n.getForm().getUsername());
+    assertEquals(userResources.message(HASHED_PASSWORD), view.i18n.getForm().getPassword());
+    assertEquals(resources.message(SIGNIN), view.i18n.getForm().getSubmit());
+    assertEquals(resources.message(property(LOCKED, TITLE)),
+        view.i18n.getErrorMessage().getTitle());
+    assertEquals(resources.message(LOCKED), view.i18n.getErrorMessage().getMessage());
+  }
+
+  @Test
+  public void localeChange() {
+    view.localeChange(mock(LocaleChangeEvent.class));
+    Locale locale = Locale.FRENCH;
+    final MessageResource resources = new MessageResource(SigninView.class, locale);
+    final MessageResource userResources = new MessageResource(User.class, locale);
+    when(ui.getLocale()).thenReturn(locale);
+    view.localeChange(mock(LocaleChangeEvent.class));
+    assertEquals(resources.message(HEADER), view.i18n.getHeader().getTitle());
+    assertEquals(resources.message(DESCRIPTION), view.i18n.getHeader().getDescription());
+    assertEquals(resources.message(FORM_TITLE), view.i18n.getForm().getTitle());
+    assertEquals(userResources.message(EMAIL), view.i18n.getForm().getUsername());
+    assertEquals(userResources.message(HASHED_PASSWORD), view.i18n.getForm().getPassword());
+    assertEquals(resources.message(SIGNIN), view.i18n.getForm().getSubmit());
+    assertEquals(resources.message(property(FAIL, TITLE)), view.i18n.getErrorMessage().getTitle());
+    assertEquals(resources.message(FAIL), view.i18n.getErrorMessage().getMessage());
+  }
+
+  @Test
+  public void getPageTitle() {
+    assertEquals(resources.message(TITLE, generalResources.message(APPLICATION_NAME)),
+        view.getPageTitle());
+  }
+
+  @Test
+  public void afterNavigationEvent_NoError() {
+    parameters.put(FAIL, null);
+
+    view.afterNavigation(afterNavigationEvent);
+
+    assertTrue(view.isError());
+  }
+
+  @Test
+  public void afterNavigationEvent_Fail() {
+    parameters.put(FAIL, null);
+
+    view.afterNavigation(afterNavigationEvent);
+
+    assertTrue(view.isError());
+  }
+
+  @Test
+  public void afterNavigationEvent_Disabled() {
+    parameters.put(DISABLED, null);
+
+    view.afterNavigation(afterNavigationEvent);
+
+    assertTrue(view.isError());
+  }
+
+  @Test
+  public void afterNavigationEvent_ExcessiveAttempts() {
+    parameters.put(LOCKED, null);
+
+    view.afterNavigation(afterNavigationEvent);
+
+    assertTrue(view.isError());
+  }
+}
