ebce806ad93060183d5588d95e4c7373fbf5a0fd
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-07-09 16:11:34 -0400

    Go to submissions view by default.

--- src/main/java/ca/qc/ircm/proview/web/MainView.java
+++ src/main/java/ca/qc/ircm/proview/web/MainView.java
@@ -1,13 +1,11 @@
 package ca.qc.ircm.proview.web;
 
 import static ca.qc.ircm.proview.user.UserRole.USER;
-import static ca.qc.ircm.proview.web.WebConstants.APPLICATION_NAME;
-import static ca.qc.ircm.proview.web.WebConstants.TITLE;
 
-import ca.qc.ircm.text.MessageResource;
-import com.vaadin.flow.component.html.H1;
+import ca.qc.ircm.proview.submission.web.SubmissionsView;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.HasDynamicTitle;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
 import javax.annotation.security.RolesAllowed;
 
@@ -16,19 +14,12 @@ import javax.annotation.security.RolesAllowed;
  */
 @Route(value = MainView.VIEW_NAME, layout = ViewLayout.class)
 @RolesAllowed({ USER })
-public class MainView extends VerticalLayout implements HasDynamicTitle {
+public class MainView extends VerticalLayout implements BeforeEnterObserver {
   public static final String VIEW_NAME = "";
   private static final long serialVersionUID = -4472228116629914718L;
-  private H1 header = new H1("Main view");
-
-  public MainView() {
-    add(header);
-  }
 
   @Override
-  public String getPageTitle() {
-    final MessageResource resources = new MessageResource(getClass(), getLocale());
-    final MessageResource generalResources = new MessageResource(WebConstants.class, getLocale());
-    return resources.message(TITLE, generalResources.message(APPLICATION_NAME));
+  public void beforeEnter(BeforeEnterEvent event) {
+    event.rerouteTo(SubmissionsView.class);
   }
 }
