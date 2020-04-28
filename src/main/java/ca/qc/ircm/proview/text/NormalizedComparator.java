a32af1e9b9db2746c6a63cea9e04f095f8eb8711
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-04-28 12:11:23 -0400

    Improves NormalizedComparator to make it usable with Vaadin.

--- src/main/java/ca/qc/ircm/proview/text/NormalizedComparator.java
+++ src/main/java/ca/qc/ircm/proview/text/NormalizedComparator.java
@@ -19,6 +19,7 @@ package ca.qc.ircm.proview.text;
 
 import static ca.qc.ircm.proview.text.Strings.normalize;
 
+import com.vaadin.flow.function.SerializableFunction;
 import java.io.Serializable;
 import java.util.Comparator;
 import java.util.Objects;
@@ -26,13 +27,34 @@ import java.util.Objects;
 /**
  * Comparator that normalizes string before comparison.
  */
-public class NormalizedComparator implements Comparator<String>, Serializable {
-  private static final long serialVersionUID = -3243433160557573573L;
+public class NormalizedComparator<T> implements Comparator<T>, Serializable {
+  private static final long serialVersionUID = -1607340161804603169L;
+  private final SerializableFunction<T, String> converter;
+
+  public NormalizedComparator(SerializableFunction<T, String> converter) {
+    this.converter = converter;
+  }
+
+  public static <T> NormalizedComparator<T> of(SerializableFunction<T, String> converter) {
+    return new NormalizedComparator<T>(converter);
+  }
 
   @Override
-  public int compare(String o1, String o2) {
-    o1 = Objects.toString(o1, "");
-    o2 = Objects.toString(o2, "");
-    return normalize(o1).compareToIgnoreCase(normalize(o2));
+  public int compare(T o1, T o2) {
+    String s1 = convert(o1);
+    String s2 = convert(o2);
+    return normalize(s1).compareToIgnoreCase(normalize(s2));
+  }
+
+  private String convert(T ot) {
+    try {
+      return Objects.toString(converter.apply(ot), "");
+    } catch (Throwable e) {
+      return "";
+    }
+  }
+
+  public SerializableFunction<T, String> getConverter() {
+    return converter;
   }
 }
--- src/test/java/ca/qc/ircm/proview/text/NormalizedComparatorTest.java
+++ src/test/java/ca/qc/ircm/proview/text/NormalizedComparatorTest.java
@@ -18,14 +18,14 @@
 package ca.qc.ircm.proview.text;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class NormalizedComparatorTest {
-  private NormalizedComparator comparator = new NormalizedComparator();
-
   @Test
-  public void normalize() {
+  public void compare_Identity() {
+    NormalizedComparator<String> comparator = new NormalizedComparator<>(s -> s);
     assertTrue(comparator.compare("bateau", "bàteau") == 0);
     assertTrue(comparator.compare("bateau", "bàteau") == 0);
     assertTrue(comparator.compare("bateau", "BÀTEAU") == 0);
@@ -68,5 +68,86 @@ public class NormalizedComparatorTest {
     assertTrue(comparator.compare("s", "ś") == 0);
     assertTrue(comparator.compare("z", "ź") == 0);
     assertTrue(comparator.compare("z", "ż") == 0);
+  }
+
+  @Test
+  public void compare_ExperimentName() {
+    NormalizedComparator<TestName> comparator = new NormalizedComparator<>(TestName::getName);
+    assertTrue(comparator.compare(name("bateau"), name("bàteau")) == 0);
+    assertTrue(comparator.compare(name("bateau"), name("bàteau")) == 0);
+    assertTrue(comparator.compare(name("bateau"), name("BÀTEAU")) == 0);
+    assertTrue(comparator.compare(name("BATEAU"), name("BÀTEAU")) == 0);
+    assertTrue(comparator.compare(name("bateau"), name("bâteau")) == 0);
+    assertTrue(comparator.compare(name("BATEAU"), name("BÂTEAU")) == 0);
+    assertTrue(comparator.compare(name("bateau"), name("bäteau")) == 0);
+    assertTrue(comparator.compare(name("BATEAU"), name("BÄTEAU")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pépin")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pèpin")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pêpin")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pëpin")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pepîn")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pepïn")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("PÉPIN")) == 0);
+    assertTrue(comparator.compare(name("pepon"), name("pepôn")) == 0);
+    assertTrue(comparator.compare(name("pepon"), name("pepön")) == 0);
+    assertTrue(comparator.compare(name("pepun"), name("pepùn")) == 0);
+    assertTrue(comparator.compare(name("pepun"), name("pepûn")) == 0);
+    assertTrue(comparator.compare(name("pepun"), name("pepün")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("pépîn")) == 0);
+    assertTrue(comparator.compare(name("pepin"), name("peqin")) < 0);
+    assertTrue(comparator.compare(name("peqin"), name("pepin")) > 0);
+    assertTrue(comparator.compare(name("pepin"), name("pepin1")) < 0);
+    assertTrue(comparator.compare(name("pepin1"), name("pepin")) > 0);
+    assertTrue(comparator.compare(name("pepin"), name("péqîn")) < 0);
+    assertTrue(comparator.compare(name("peqin"), name("pépîn")) > 0);
+    assertTrue(comparator.compare(null, name("pepin")) < 0);
+    assertTrue(comparator.compare(name(null), name("pepin")) < 0);
+    assertTrue(comparator.compare(name("pepin"), null) > 0);
+    assertTrue(comparator.compare(name("pepin"), name(null)) > 0);
+    assertTrue(comparator.compare(name(""), name("pepin")) < 0);
+    assertTrue(comparator.compare(name("pepin"), name("")) > 0);
+    // Test Polish, out of curiosity.
+    assertTrue(comparator.compare(name("a"), name("ą")) == 0);
+    assertTrue(comparator.compare(name("c"), name("ć")) == 0);
+    assertTrue(comparator.compare(name("e"), name("ę")) == 0);
+    // Doesn't work because ł in Unicode is not l with a slash, but its own character.
+    //assertTrue(comparator.compare(name("l"), name("ł")) == 0);
+    assertTrue(comparator.compare(name("n"), name("ń")) == 0);
+    assertTrue(comparator.compare(name("o"), name("ó")) == 0);
+    assertTrue(comparator.compare(name("s"), name("ś")) == 0);
+    assertTrue(comparator.compare(name("z"), name("ź")) == 0);
+    assertTrue(comparator.compare(name("z"), name("ż")) == 0);
+  }
+
+  @Test
+  public void getConverter_Identity() {
+    NormalizedComparator<String> comparator = new NormalizedComparator<>(s -> s);
+    assertEquals("test", comparator.getConverter().apply("test"));
+    assertEquals("abc", comparator.getConverter().apply("abc"));
+    assertEquals("pépîn", comparator.getConverter().apply("pépîn"));
+    assertEquals(null, comparator.getConverter().apply(null));
+  }
+
+  @Test
+  public void getConverter_ExperimentName() {
+    NormalizedComparator<TestName> comparator = new NormalizedComparator<>(TestName::getName);
+    assertEquals("test", comparator.getConverter().apply(name("test")));
+    assertEquals("abc", comparator.getConverter().apply(name("abc")));
+    assertEquals("pépîn", comparator.getConverter().apply(name("pépîn")));
+    assertEquals(null, comparator.getConverter().apply(name(null)));
+  }
+
+  private TestName name(String name) {
+    TestName testName = new TestName();
+    testName.name = name;
+    return testName;
+  }
+
+  private static class TestName {
+    private String name;
+
+    public String getName() {
+      return name;
+    }
   }
 }
