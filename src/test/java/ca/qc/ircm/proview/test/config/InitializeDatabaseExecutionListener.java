31c004e8255fd403c43d2f763a964c95a4614765
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-30 10:31:57 -0400

    Removes unused method in InitializeDatabaseExecutionListener.

--- src/test/java/ca/qc/ircm/proview/test/config/InitializeDatabaseExecutionListener.java
+++ src/test/java/ca/qc/ircm/proview/test/config/InitializeDatabaseExecutionListener.java
@@ -17,12 +17,7 @@
 
 package ca.qc.ircm.proview.test.config;
 
-import ca.qc.ircm.proview.user.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
 /**
@@ -31,8 +26,6 @@ import org.springframework.test.context.TestExecutionListener;
 @Order(5001)
 public class InitializeDatabaseExecutionListener
     implements TestExecutionListener, InjectDependencies {
-  private static final Logger logger =
-      LoggerFactory.getLogger(InitializeDatabaseExecutionListener.class);
   /**
    * Matches pass1.
    */
@@ -45,33 +38,4 @@ public class InitializeDatabaseExecutionListener
   @SuppressWarnings("checkstyle:linelength")
   public static final String PASSWORD_PASS2 =
       "$2a$10$JU0aj7Cc/7sWVkFXoHbWTuvVWEAwXFT1EhCX4S6Aa9JfSsKqLP8Tu";
-  @Autowired
-  private UserRepository userRepository;
-
-  @Override
-  @SuppressWarnings("checkstyle:linelength")
-  public void beforeTestMethod(TestContext testContext) throws Exception {
-    logger.debug("Initializes database");
-
-    /*
-    User user = userRepository.findById(1L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(4, ChronoUnit.DAYS));
-    userRepository.save(user);
-    user = userRepository.findById(2L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(2, ChronoUnit.HOURS));
-    userRepository.save(user);
-    user = userRepository.findById(3L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(10, ChronoUnit.DAYS));
-    userRepository.save(user);
-    user = userRepository.findById(4L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(21, ChronoUnit.DAYS));
-    userRepository.save(user);
-    user = userRepository.findById(5L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(20, ChronoUnit.MINUTES));
-    userRepository.save(user);
-    user = userRepository.findById(6L).orElse(null);
-    user.setLastSignAttempt(LocalDateTime.now().minus(20, ChronoUnit.MINUTES));
-    userRepository.save(user);
-    */
-  }
 }
