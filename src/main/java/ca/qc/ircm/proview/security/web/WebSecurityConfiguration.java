409a25cb23dd09e6146f56898f9765489f605f6a
Christian Poitras <christian.poitras@ircm.qc.ca>
2022-12-08 19:53:45 -0500

    Fixes deprecated usage of newInstance() method.

--- src/main/java/ca/qc/ircm/proview/security/web/WebSecurityConfiguration.java
+++ src/main/java/ca/qc/ircm/proview/security/web/WebSecurityConfiguration.java
@@ -230,7 +230,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     // Used for TestBench.
     try {
       Class<?> clazz = Class.forName("ca.qc.ircm.proview.test.config.TestBenchSecurityFilter");
-      http.addFilterBefore((Filter) clazz.newInstance(), SecurityContextPersistenceFilter.class);
+      http.addFilterBefore((Filter) clazz.getDeclaredConstructor().newInstance(),
+          SecurityContextPersistenceFilter.class);
     } catch (ClassNotFoundException e) {
       // Ignore, not running unit tests.
     }
