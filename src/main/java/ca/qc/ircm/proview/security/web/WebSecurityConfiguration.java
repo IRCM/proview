bd16a7b70ca0d0dc13d243b49f4c46d8776ed446
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-04-05 12:16:53 -0400

    Replaces deprecated SecurityContextPersistenceFilter with SecurityContextHolderFilter.

--- src/main/java/ca/qc/ircm/proview/security/web/WebSecurityConfiguration.java
+++ src/main/java/ca/qc/ircm/proview/security/web/WebSecurityConfiguration.java
@@ -50,7 +50,7 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 /**
  * Security configuration.
@@ -191,7 +191,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     try {
       Class<?> clazz = Class.forName("ca.qc.ircm.proview.test.config.TestBenchSecurityFilter");
       http.addFilterBefore((Filter) clazz.getDeclaredConstructor().newInstance(),
-          SecurityContextPersistenceFilter.class);
+          SecurityContextHolderFilter.class);
     } catch (ClassNotFoundException e) {
       // Ignore, not running unit tests.
     }
