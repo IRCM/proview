558aa188f6a328ab664686e1d2200a910682570f
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-01-12 15:41:39 -0500

    Merges LaboratoryDialogPresenter into LaboratoryDialog.

--- src/test/java/ca/qc/ircm/proview/user/web/LaboratoryDialogTest.java
+++ src/test/java/ca/qc/ircm/proview/user/web/LaboratoryDialogTest.java
@@ -20,37 +20,47 @@ package ca.qc.ircm.proview.user.web;
 import static ca.qc.ircm.proview.Constants.CANCEL;
 import static ca.qc.ircm.proview.Constants.ENGLISH;
 import static ca.qc.ircm.proview.Constants.FRENCH;
+import static ca.qc.ircm.proview.Constants.REQUIRED;
 import static ca.qc.ircm.proview.Constants.SAVE;
-import static ca.qc.ircm.proview.test.utils.VaadinTestUtils.clickButton;
+import static ca.qc.ircm.proview.test.utils.VaadinTestUtils.findValidationStatusByField;
 import static ca.qc.ircm.proview.test.utils.VaadinTestUtils.validateIcon;
 import static ca.qc.ircm.proview.user.LaboratoryProperties.NAME;
 import static ca.qc.ircm.proview.user.web.LaboratoryDialog.HEADER;
 import static ca.qc.ircm.proview.user.web.LaboratoryDialog.ID;
+import static ca.qc.ircm.proview.user.web.LaboratoryDialog.SAVED;
 import static ca.qc.ircm.proview.user.web.LaboratoryDialog.id;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import ca.qc.ircm.proview.AppResources;
 import ca.qc.ircm.proview.Constants;
-import ca.qc.ircm.proview.test.config.AbstractKaribuTestCase;
 import ca.qc.ircm.proview.test.config.ServiceTestAnnotations;
 import ca.qc.ircm.proview.user.Laboratory;
 import ca.qc.ircm.proview.user.LaboratoryRepository;
+import ca.qc.ircm.proview.user.LaboratoryService;
 import ca.qc.ircm.proview.web.SavedEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.data.binder.BinderValidationStatus;
+import com.vaadin.flow.data.binder.BindingValidationStatus;
+import com.vaadin.testbench.unit.SpringUIUnitTest;
 import java.util.Locale;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 
 /**
@@ -58,14 +68,14 @@ import org.springframework.security.test.context.support.WithUserDetails;
  */
 @ServiceTestAnnotations
 @WithUserDetails("proview@ircm.qc.ca")
-public class LaboratoryDialogTest extends AbstractKaribuTestCase {
+public class LaboratoryDialogTest extends SpringUIUnitTest {
   private LaboratoryDialog dialog;
-  @Mock
-  private LaboratoryDialogPresenter presenter;
-  @Mock
-  private Laboratory laboratory;
+  @MockBean
+  private LaboratoryService service;
   @Mock
   private ComponentEventListener<SavedEvent<LaboratoryDialog>> savedListener;
+  @Captor
+  private ArgumentCaptor<Laboratory> laboratoryCaptor;
   @Autowired
   private LaboratoryRepository repository;
   private Locale locale = ENGLISH;
@@ -78,14 +88,11 @@ public class LaboratoryDialogTest extends AbstractKaribuTestCase {
    */
   @BeforeEach
   public void beforeTest() {
-    ui.setLocale(locale);
-    dialog = new LaboratoryDialog(presenter);
-    dialog.init();
-  }
-
-  @Test
-  public void presenter_Init() {
-    verify(presenter).init(dialog);
+    UI.getCurrent().setLocale(locale);
+    UsersView view = navigate(UsersView.class);
+    test(view.users).select(0);
+    test(view.viewLaboratory).click();
+    dialog = $(LaboratoryDialog.class).first();
   }
 
   @Test
@@ -101,28 +108,23 @@ public class LaboratoryDialogTest extends AbstractKaribuTestCase {
 
   @Test
   public void labels() {
-    dialog.localeChange(mock(LocaleChangeEvent.class));
     assertEquals(resources.message(HEADER, 0), dialog.getHeaderTitle());
     assertEquals(laboratoryResources.message(NAME), dialog.name.getLabel());
     assertEquals(webResources.message(SAVE), dialog.save.getText());
     assertEquals(webResources.message(CANCEL), dialog.cancel.getText());
-    verify(presenter).localeChange(locale);
   }
 
   @Test
   public void localeChange() {
-    dialog.localeChange(mock(LocaleChangeEvent.class));
     Locale locale = FRENCH;
     final AppResources resources = new AppResources(LaboratoryDialog.class, locale);
     final AppResources laboratoryResources = new AppResources(Laboratory.class, locale);
     final AppResources webResources = new AppResources(Constants.class, locale);
-    ui.setLocale(locale);
-    dialog.localeChange(mock(LocaleChangeEvent.class));
+    UI.getCurrent().setLocale(locale);
     assertEquals(resources.message(HEADER, 0), dialog.getHeaderTitle());
     assertEquals(laboratoryResources.message(NAME), dialog.name.getLabel());
     assertEquals(webResources.message(SAVE), dialog.save.getText());
     assertEquals(webResources.message(CANCEL), dialog.cancel.getText());
-    verify(presenter).localeChange(locale);
   }
 
   @Test
@@ -141,67 +143,102 @@ public class LaboratoryDialogTest extends AbstractKaribuTestCase {
 
   @Test
   public void getLaboratory() {
-    when(presenter.getLaboratory()).thenReturn(laboratory);
+    Laboratory laboratory = repository.findById(2L).get();
+    dialog.setLaboratory(laboratory);
     assertEquals(laboratory, dialog.getLaboratory());
-    verify(presenter).getLaboratory();
   }
 
   @Test
-  public void setLaboratory_NewLaboratory() {
-    Laboratory laboratory = new Laboratory();
-    when(presenter.getLaboratory()).thenReturn(laboratory);
-
-    dialog.localeChange(mock(LocaleChangeEvent.class));
-    dialog.setLaboratory(laboratory);
-
-    verify(presenter).setLaboratory(laboratory);
-    assertEquals(resources.message(HEADER, 0), dialog.getHeaderTitle());
-  }
-
-  @Test
-  public void setLaboratory_Laboratory() {
+  public void setLaboratory() {
     Laboratory laboratory = repository.findById(2L).get();
-    when(presenter.getLaboratory()).thenReturn(laboratory);
-
-    dialog.localeChange(mock(LocaleChangeEvent.class));
     dialog.setLaboratory(laboratory);
-
-    verify(presenter).setLaboratory(laboratory);
     assertEquals(resources.message(HEADER, 1, laboratory.getName()), dialog.getHeaderTitle());
-  }
-
-  @Test
-  public void setLaboratory_LaboratoryBeforeLocaleChange() {
-    Laboratory laboratory = repository.findById(2L).get();
-    when(presenter.getLaboratory()).thenReturn(laboratory);
-
-    dialog.setLaboratory(laboratory);
-    dialog.localeChange(mock(LocaleChangeEvent.class));
-
-    verify(presenter).setLaboratory(laboratory);
-    assertEquals(resources.message(HEADER, 1, laboratory.getName()), dialog.getHeaderTitle());
+    assertEquals(laboratory.getName(), dialog.name.getValue());
+    assertFalse(dialog.name.isReadOnly());
+    assertTrue(dialog.save.isVisible());
+    assertTrue(dialog.cancel.isVisible());
   }
 
   @Test
   public void setLaboratory_Null() {
-    dialog.localeChange(mock(LocaleChangeEvent.class));
     dialog.setLaboratory(null);
-
-    verify(presenter).setLaboratory(null);
     assertEquals(resources.message(HEADER, 0), dialog.getHeaderTitle());
+    assertEquals("", dialog.name.getValue());
+    assertFalse(dialog.name.isReadOnly());
+    assertTrue(dialog.save.isVisible());
+    assertTrue(dialog.cancel.isVisible());
   }
 
   @Test
-  public void save() {
-    clickButton(dialog.save);
+  public void save_EmptyName() {
+    dialog.addSavedListener(savedListener);
+    dialog.name.setValue("");
 
-    verify(presenter).save(locale);
+    test(dialog.save).click();
+
+    BinderValidationStatus<Laboratory> status = dialog.validate();
+    assertFalse(status.isOk());
+    Optional<BindingValidationStatus<?>> optionalError =
+        findValidationStatusByField(status, dialog.name);
+    assertTrue(optionalError.isPresent());
+    BindingValidationStatus<?> error = optionalError.get();
+    assertEquals(Optional.of(webResources.message(REQUIRED)), error.getMessage());
+    verify(service, never()).save(any());
+    assertFalse($(Notification.class).exists());
+    assertTrue(dialog.isOpened());
+    verify(savedListener, never()).onComponentEvent(any());
+  }
+
+  @Test
+  public void save_New() {
+    dialog.addSavedListener(savedListener);
+    String name = "My lab";
+    dialog.name.setValue(name);
+
+    test(dialog.save).click();
+
+    verify(service).save(laboratoryCaptor.capture());
+    Laboratory laboratory = laboratoryCaptor.getValue();
+    assertNull(laboratory.getId());
+    assertEquals(name, laboratory.getName());
+    assertNull(laboratory.getDirector());
+    Notification notification = $(Notification.class).first();
+    assertEquals(resources.message(SAVED, name), test(notification).getText());
+    assertFalse(dialog.isOpened());
+    verify(savedListener).onComponentEvent(any());
+  }
+
+  @Test
+  public void save_Update() {
+    dialog.addSavedListener(savedListener);
+    dialog.setLaboratory(repository.findById(2L).get());
+    String name = "My lab";
+    dialog.name.setValue(name);
+
+    test(dialog.save).click();
+
+    verify(service).save(laboratoryCaptor.capture());
+    Laboratory laboratory = laboratoryCaptor.getValue();
+    assertEquals((Long) 2L, laboratory.getId());
+    assertEquals(name, laboratory.getName());
+    assertEquals("Benoit Coulombe", laboratory.getDirector());
+    Notification notification = $(Notification.class).first();
+    assertEquals(resources.message(SAVED, name), test(notification).getText());
+    assertFalse(dialog.isOpened());
+    verify(savedListener).onComponentEvent(any());
   }
 
   @Test
   public void cancel() {
-    clickButton(dialog.cancel);
+    dialog.addSavedListener(savedListener);
+    String name = "My lab";
+    dialog.name.setValue(name);
 
-    verify(presenter).cancel();
+    test(dialog.cancel).click();
+
+    verify(service, never()).save(any());
+    assertFalse($(Notification.class).exists());
+    assertFalse(dialog.isOpened());
+    verify(savedListener, never()).onComponentEvent(any());
   }
 }
