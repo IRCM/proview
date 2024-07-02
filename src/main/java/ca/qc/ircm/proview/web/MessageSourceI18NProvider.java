144bf94ae0bebe8967f8f9f7cac766db44da7d76
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-07-02 16:24:41 -0400

    Link MessageSource instance to Vaadin using MessageSourceI18NProvider.

---
+++ src/main/java/ca/qc/ircm/proview/web/MessageSourceI18NProvider.java
@@ -0,0 +1,44 @@
+package ca.qc.ircm.proview.web;
+
+import ca.qc.ircm.proview.Constants;
+import com.vaadin.flow.i18n.I18NProvider;
+import java.util.List;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.stereotype.Component;
+
+/**
+ * {@link I18NProvider} implementation that uses {@link MessageSource}.
+ */
+@Component
+public class MessageSourceI18NProvider implements I18NProvider {
+  private static final Logger logger = LoggerFactory.getLogger(MessageSourceI18NProvider.class);
+  /**
+   * {@link MessageSource} from Spring.
+   */
+  private final MessageSource messageSource;
+
+  @Autowired
+  protected MessageSourceI18NProvider(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @Override
+  public List<Locale> getProvidedLocales() {
+    return Constants.getLocales();
+  }
+
+  @Override
+  public String getTranslation(String key, Locale locale, Object... params) {
+    try {
+      return messageSource.getMessage(key, params, locale);
+    } catch (NoSuchMessageException e) {
+      logger.warn("Could not find message for key {}", key);
+      return "!{" + key + "}!";
+    }
+  }
+}
