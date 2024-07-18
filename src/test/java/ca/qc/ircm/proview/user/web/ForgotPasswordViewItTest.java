ecef4ef3886fd2e63a0d334b0ea0ead47d3d5d4e
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-07-18 14:46:48 -0400

    Use getTranslation in ForgotPasswordView.

--- src/test/java/ca/qc/ircm/proview/user/web/ForgotPasswordViewItTest.java
+++ src/test/java/ca/qc/ircm/proview/user/web/ForgotPasswordViewItTest.java
@@ -2,12 +2,12 @@ package ca.qc.ircm.proview.user.web;
 
 import static ca.qc.ircm.proview.Constants.APPLICATION_NAME;
 import static ca.qc.ircm.proview.Constants.TITLE;
+import static ca.qc.ircm.proview.Constants.messagePrefix;
 import static ca.qc.ircm.proview.user.web.ForgotPasswordView.SAVED;
 import static ca.qc.ircm.proview.user.web.ForgotPasswordView.VIEW_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import ca.qc.ircm.proview.AppResources;
 import ca.qc.ircm.proview.Constants;
 import ca.qc.ircm.proview.test.config.AbstractTestBenchTestCase;
 import ca.qc.ircm.proview.test.config.TestBenchTestAnnotations;
@@ -20,16 +20,20 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 
 /**
  * Integration tests for {@link ForgotPasswordView}.
  */
 @TestBenchTestAnnotations
 public class ForgotPasswordViewItTest extends AbstractTestBenchTestCase {
+  private static final String MESSAGES_PREFIX = messagePrefix(ForgotPasswordView.class);
   @SuppressWarnings("unused")
   private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordViewItTest.class);
   @Autowired
   private ForgotPasswordRepository repository;
+  @Autowired
+  private MessageSource messageSource;
   private String email = "christopher.anderson@ircm.qc.ca";
 
   private void open() {
@@ -62,8 +66,9 @@ public class ForgotPasswordViewItTest extends AbstractTestBenchTestCase {
     view.save().click();
 
     NotificationElement notification = $(NotificationElement.class).waitForFirst();
-    AppResources resources = this.resources(ForgotPasswordView.class);
-    assertEquals(resources.message(SAVED, email), notification.getText());
+    assertEquals(
+        messageSource.getMessage(MESSAGES_PREFIX + SAVED, new Object[] { email }, currentLocale()),
+        notification.getText());
     List<ForgotPassword> forgotPasswords = repository.findByUserEmail(email);
     assertEquals(4, forgotPasswords.size());
     $(SigninViewElement.class).waitForFirst();
