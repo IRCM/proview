c2807f8842c8d90e6ba2709d33761525bd03bb8f
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-01-30 11:47:33 -0500

    Removes unused import.

--- src/test/java/ca/qc/ircm/proview/sample/SampleServiceTest.java
+++ src/test/java/ca/qc/ircm/proview/sample/SampleServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
