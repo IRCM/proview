20360e1d7efe645fe2e7ecf83a1ed39e52ecb060
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-02-10 16:18:25 -0500

    Adds GeneratePropertyNames annotation and `passwords do not match` error message to Passwords class.

--- src/main/java/ca/qc/ircm/proview/user/web/Passwords.java
+++ src/main/java/ca/qc/ircm/proview/user/web/Passwords.java
@@ -1,12 +1,15 @@
 package ca.qc.ircm.proview.user.web;
 
+import ca.qc.ircm.processing.GeneratePropertyNames;
 import org.springframework.lang.Nullable;
 
 /**
  * Stores password and a confirmation that should match the password.
  */
+@GeneratePropertyNames
 public class Passwords {
 
+  public static final String NOT_MATCH = "notMatch";
   private String password;
   private String confirmPassword;
 
