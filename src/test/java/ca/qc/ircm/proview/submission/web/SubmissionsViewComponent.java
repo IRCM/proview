ccce299f6b374519e8038d065cb386e953c09b0b
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-08-24 15:55:14 -0400

    Use selenium for IT requiring browser in submission package

--- src/test/java/ca/qc/ircm/proview/submission/web/SubmissionsViewComponent.java
+++ src/test/java/ca/qc/ircm/proview/submission/web/SubmissionsViewComponent.java
@@ -3,8 +3,6 @@ package ca.qc.ircm.proview.submission.web;
 import static ca.qc.ircm.proview.submission.web.SubmissionsView.ID;
 
 import ca.qc.ircm.proview.test.config.SeleniumComponent;
-import com.vaadin.testbench.annotations.Attribute;
-import com.vaadin.testbench.elementsbase.Element;
 import java.util.function.Function;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,8 +11,6 @@ import org.openqa.selenium.WebElement;
 /**
  * Web element for {@link SubmissionsView}.
  */
-@Element("vaadin-vertical-layout")
-@Attribute(name = "id", value = SubmissionsView.ID)
 public class SubmissionsViewComponent extends SeleniumComponent {
 
   public SubmissionsViewComponent(WebElement element) {
