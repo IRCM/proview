b8604c1646d4ca654ba902a013eb41e18765e675
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-08-25 14:35:42 -0400

    Allow to skip selenium tests

--- src/test/java/ca/qc/ircm/proview/test/config/SeleniumTestAnnotations.java
+++ src/test/java/ca/qc/ircm/proview/test/config/SeleniumTestAnnotations.java
@@ -21,7 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration-test")
-@TestExecutionListeners(value = {FixSecurityContextHolderStrategyExecutionListener.class,
+@TestExecutionListeners(value = {SkipSeleniumTestExecutionListener.class,
+    FixSecurityContextHolderStrategyExecutionListener.class,
     SeleniumSecurityFilter.class}, mergeMode = MergeMode.MERGE_WITH_DEFAULTS)
 @Execution(ExecutionMode.SAME_THREAD)
 @Transactional
